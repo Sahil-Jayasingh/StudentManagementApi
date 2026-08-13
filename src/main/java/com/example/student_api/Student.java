8b60fafb9e1f58e31f02345f36f146ca576310bf
Sahil-Jayasingh <227570893+Sahil-Jayasingh@users.noreply.github.com>
2026-08-13 21:32:19 +0530

    Added Dept

--- src/main/java/com/example/student_api/Student.java
+++ src/main/java/com/example/student_api/Student.java
@@ -9,11 +9,13 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+    private String dept;
     private String name;
 
     public Long getId() { return id; }
     public String getName() { return name; }
+    public String getDept() { return dept; }
     public void setId(Long id) { this.id = id; }
     public void setName(String name) { this.name = name; }
+    public void setDept(String dept) { this.dept = dept; }
 }
