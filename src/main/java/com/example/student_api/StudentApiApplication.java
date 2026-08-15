fe840a4c6866069166220ac28356668ebb343097
Sahil-Jayasingh <227570893+Sahil-Jayasingh@users.noreply.github.com>
2026-08-15 12:07:14 +0530

    Fixes

--- src/main/java/com/example/student_api/StudentApiApplication.java
+++ src/main/java/com/example/student_api/StudentApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApiApplication.class, args);
 	}
